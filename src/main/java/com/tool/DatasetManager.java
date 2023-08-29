1dc35af478748201fedf1ed35fa71d168b006328
Giuseppe Porcaro <51061817+GiuseppePorcaro@users.noreply.github.com>
2023-08-29 04:52:49 +0200

    fix

--- src/main/java/com/tool/DatasetManager.java
+++ src/main/java/com/tool/DatasetManager.java
@@ -8,13 +8,24 @@ import java.sql.*;
 * */
 public class DatasetManager {
 
+    private String dbPath = "/Volumes/SDDPeppe/Università/Libri_università/Magistrale/Tesi_magistrale/Web_Test_Generation/Crawls_complete/GroundTruthModels/gs.db";
+
     public void main(){
 
         try {
             Class.forName("org.sqlite.JDBC");
-            Connection conn = DriverManager.getConnection("jdbc:sqlite:/Volumes/SDDPeppe/Università/Libri_università/Magistrale/Tesi_magistrale/Web_Test_Generation/Crawls_complete/GroundTruthModels/gs.db");
+            Connection conn = DriverManager.getConnection("jdbc:sqlite:"+dbPath);
             Statement stat = conn.createStatement();
 
+
+            /*
+            * Cose da fare per popolare il dataset:
+            * 1) creare una nuova colonna per tipo di similarità nel file db
+            * 2) Iterare sulla tabella nearduplicates: per ogni record prendere appname, state1, state2
+            * 3) Ricreare il percorso per arrivare a state1.html e state2.html tramite appname, state1 e state2
+            * 4) Presi gli html, calcolare il kernel normalizzato
+            *
+            * */
             ResultSet rs = stat.executeQuery("SELECT state1, state2, HUMAN_CLASSIFICATION from nearduplicates where appname=\"addressbook\" LIMIT 15;");
 
             while (rs.next()){
