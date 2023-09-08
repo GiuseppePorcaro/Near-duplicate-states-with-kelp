dcdcb3a91710a36417181856955a0976fab0465a
Giuseppe Porcaro <51061817+GiuseppePorcaro@users.noreply.github.com>
2023-09-08 22:56:53 +0200

    fixed a query and some refactoring

--- src/main/java/com/tool/DatasetManager.java
+++ src/main/java/com/tool/DatasetManager.java
@@ -1,17 +1,11 @@
 package com.tool;
 
-import com.tool.Trees.TreeFactory;
 import com.tool.similarity.AllAttributesJaccardSimilarity;
-import it.uniroma2.sag.kelp.data.representation.tree.TreeRepresentation;
-import it.uniroma2.sag.kelp.kernel.DirectKernel;
-import it.uniroma2.sag.kelp.kernel.tree.PartialTreeKernel;
-import it.uniroma2.sag.kelp.kernel.tree.SmoothedPartialTreeKernel;
 
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
-import static com.tool.representations.ManageTreeRepresentation.popolateTree;
 import static java.lang.Math.round;
 
 
@@ -22,27 +16,23 @@ import static java.lang.Math.round;
 
 public class DatasetManager {
 
-    //private String folderPath = "F:\\Università\\Libri_università\\Magistrale\\Tesi_magistrale\\Web_Test_Generation\\Crawls_complete\\GroundTruthModels"; path per windows
-    //private String folderPath = "/Volumes/SDDPeppe/Università/Libri_università/Magistrale/Tesi_magistrale/Web_Test_Generation/Crawls_complete/GroundTruthModels/";
-
-
     public void main() throws InterruptedException {
 
         int numCores = Runtime.getRuntime().availableProcessors();
         int numRows = 97490;
         int slice = round(numRows/numCores) + 1;
-        System.out.println(slice);
         int start = 0;
+        String folderPath = "/run/media/giuseppeporcaro/SDDPeppe/Università/Libri_università/Magistrale/Tesi_magistrale/Web_Test_Generation/Crawls_complete/GroundTruthModels";
+        String datasetDB = "gs_full_dom.db";
 
         List<Thread> threads = new ArrayList<>();
 
         for(int i = 0; i < numCores; i++){
             start = i*slice;
-            Thread t = new Thread(new MyRunnable(slice,start,"/run/media/giuseppeporcaro/SDDPeppe/Università/Libri_università/Magistrale/Tesi_magistrale/Web_Test_Generation/Crawls_complete/GroundTruthModels/","gs_full_dom.db",i));
+            Thread t = new Thread(new MyRunnable(slice,start,folderPath,datasetDB,i));
 
             t.start();
             threads.add(t);
-
         }
 
         for(Thread thread: threads){
@@ -50,84 +40,129 @@ public class DatasetManager {
         }
     }
 
+    /*
+    *
+    * Parallelizza il calcolo e la scrittura delle similarità sul database
+    *
+    * */
     public class MyRunnable implements Runnable{
 
         private static Integer counter = 0;
-
         private final Object lock = new Object();
         private Integer numThread;
         private Integer slice;
         private Integer start;
 
         private String folderPath;
-        private String db;
+        private String datasetDB;
         @Override
         public void run() {
 
             long startTime = System.currentTimeMillis();
             try {
                 Class.forName("org.sqlite.JDBC");
-                Connection conn = DriverManager.getConnection("jdbc:sqlite:"+folderPath+"//"+db);
+                Connection conn = DriverManager.getConnection("jdbc:sqlite:"+folderPath+"/"+ datasetDB);
                 Statement stat = conn.createStatement();
 
                 ResultSet rs = stat.executeQuery("SELECT appname, crawl,state1, state2 from nearduplicates order by appname, crawl, state1, state2 limit "+start+","+slice+";");
 
+                List<DatasetRow> entities = new ArrayList<>();
 
-                ResultSet a = rs;
+                while(rs.next()){
+                    entities.add(new DatasetRow(rs.getString("appname"),rs.getString("crawl"),rs.getString("state1"),rs.getString("state2")));
+                }
+
                 rs.close();
                 conn.close();
 
-                while (a.next()){
+
+                for(DatasetRow entity: entities){
                     SimilarityTool similarityTool = new SimilarityTool(new AllAttributesJaccardSimilarity(),0.4f,0.4f,1,0.05f,null);
 
-                    String appName = rs.getString("appname");
-                    String crawl = rs.getString("crawl");
-                    String state1 = rs.getString("state1");
-                    String state2 = rs.getString("state2");
+                    String appName = entity.getAppname();
+                    String crawl = entity.getCrawl();
+                    String state1 = entity.getState1();
+                    String state2 = entity.getState2();
 
                     String pathHTML1 = folderPath+"/"+appName+"/"+crawl+"/doms/"+state1+".html";
                     String pathHTML2 = folderPath+"/"+appName+"/"+crawl+"/doms/"+state2+".html";
 
                     float kernel = similarityTool.computeKernelNormalized(pathHTML1,pathHTML2,"all");
-                    //System.out.println("Path state1: "+pathHTML1+"\nPath state2: "+pathHTML2+"\n");
-
-                    synchronized (lock){
-                        counter++;
-                    }
-                    System.out.println(appName+" "+crawl+" "+state1+" "+state2+" - "+kernel + " | "+counter+ " | Thread:  "+numThread);
 
                     if(counter % 10 == 0){
                         long elapsedTime = (startTime - System.currentTimeMillis())/1000;
                         System.out.println("Thread: "+numThread+" | seconds: "+elapsedTime);
                     }
 
-                    String query = "UPDATE nearduplicates SET ATTRIBUTE_SIM=? where appname= ? and crawl = ? and state1 = ? and state2 = ?;";
-                    PreparedStatement statement = conn.prepareStatement(query);
-                    statement.setFloat(1, kernel);
-                    statement.setString(2, appName);
-                    statement.setString(3, crawl);
-                    statement.setString(4,state1);
-                    statement.setString(4,state2);
+                    synchronized (lock){
+                        DriverManager.setLoginTimeout(10);
+                        Connection connUpdate = DriverManager.getConnection("jdbc:sqlite:"+folderPath+"/"+ datasetDB);
+                        connUpdate.setAutoCommit(true);
 
-                    statement.executeUpdate();
+                        String query = "UPDATE nearduplicates SET ATTRIBUTE_SIM=? where appname=? and crawl=? and state1=? and state2=?;";
+                        PreparedStatement statement = connUpdate.prepareStatement(query);
+                        statement.setFloat(1, kernel);
+                        statement.setString(2, appName);
+                        statement.setString(3, crawl);
+                        statement.setString(4,state1);
+                        statement.setString(5,state2);
+
+                        statement.executeUpdate();
+
+                        statement.close();
+                        connUpdate.close();
+
+                        counter++;
+                        System.out.println(appName+" "+crawl+" "+state1+" "+state2+" - "+kernel + " |\tThread: "+numThread+ " - Pair N°:  "+counter);
+                    }
                 }
 
-            } catch (ClassNotFoundException e) {
-                throw new RuntimeException(e);
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
             } catch (Exception e) {
                 throw new RuntimeException(e);
             }
 
+
         }
 
-        public MyRunnable(Integer slice, Integer start, String folderPath, String db, Integer numThread) {
+        public MyRunnable(Integer slice, Integer start, String folderPath, String datasetDB, Integer numThread) {
             this.slice = slice;
             this.start = start;
             this.folderPath = folderPath;
-            this.db = db;
+            this.datasetDB = datasetDB;
             this.numThread = numThread;
         }
     }
+
+    public class DatasetRow {
+        private String appname;
+        private String crawl;
+        private String state1;
+        private String state2;
+
+        public DatasetRow(String appname, String crawl, String state1, String state2) {
+            this.appname = appname;
+            this.crawl = crawl;
+            this.state1 = state1;
+            this.state2 = state2;
+        }
+
+        public String getAppname() {
+            return appname;
+        }
+
+        public String getCrawl() {
+            return crawl;
+        }
+
+        public String getState1() {
+            return state1;
+        }
+
+        public String getState2() {
+            return state2;
+        }
+    }
 }
+
+//private String folderPath = "F:\\Università\\Libri_università\\Magistrale\\Tesi_magistrale\\Web_Test_Generation\\Crawls_complete\\GroundTruthModels"; path per windows
+//private String folderPath = "/Volumes/SDDPeppe/Università/Libri_università/Magistrale/Tesi_magistrale/Web_Test_Generation/Crawls_complete/GroundTruthModels/"; path MacOs
--- src/main/java/com/tool/MainClass.java
+++ src/main/java/com/tool/MainClass.java
@@ -5,6 +5,10 @@ import com.tool.debug.DebugClass;
 import com.tool.similarity.AllAttributesJaccardSimilarity;
 import it.uniroma2.sag.kelp.data.representation.tree.TreeRepresentation;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
 public class MainClass {
 
     public static void main(String args[]) throws Exception {
@@ -19,25 +23,4 @@ public class MainClass {
         /*DebugClass debugClass = new DebugClass();
         debugClass.start();*/
     }
-
-    public static void mainForScriptPython(String args[]) throws Exception {
-        SimilarityTool similarityTool = new SimilarityTool(new AllAttributesJaccardSimilarity(),0.4f,0.4f,1,0.01f,null);
-
-        String folderPath = "/Volumes/SDDPeppe/Università/Libri_università/Magistrale/Tesi_magistrale/Web_Test_Generation/Crawls_complete/GroundTruthModels/";
-        String appName = args[0];
-        String crawl = args[1];
-        String state1 = args[2];
-        String state2 = args[3];
-        Integer counter = Integer.parseInt(args[4]);
-        //System.out.println(appName+" "+crawl+" "+state1+" "+state2);
-
-        String pathHTML1 = folderPath+"/"+appName+"/"+crawl+"/doms/"+state1+".html";
-        String pathHTML2 = folderPath+"/"+appName+"/"+crawl+"/doms/"+state2+".html";
-
-        float kernel = similarityTool.computeKernelNormalized(pathHTML1,pathHTML2,"treeForCrawl");
-        //System.out.println("Path state1: "+pathHTML1+"\nPath state2: "+pathHTML2+"\n");
-        System.out.println(appName+" "+crawl+" "+state1+" "+state2+" - | "+kernel+ " | "+counter);
-    }
-
-
 }
