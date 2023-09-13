33bdd4c9bbda154d4c5fd9ed34fd834e4956c17a
Giuseppe Porcaro <51061817+GiuseppePorcaro@users.noreply.github.com>
2023-09-13 03:57:35 +0200

    implemented a first classificator (svc)

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -2,6 +2,11 @@ import pandas as pd
 import numpy as np
 import sqlite3
 import csv
+from sklearn.model_selection import train_test_split
+from sklearn import svm
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn import metrics
 
 def main():
     print("Caricamento dataset...")
@@ -15,9 +20,25 @@ def main():
     print("\n\n")
     print("Sample:\n",datasetX)
     print("Target:\n",datasetY)
+    
+    X_train, X_test, y_train, y_test = train_test_split(datasetX, datasetY , test_size=0.25, random_state=0)
+
+    print("X_train shape: ",X_train.shape)
+    print("X_test shape: ",X_test.shape)
+    print("y_train shape: ",y_train.shape)
+    print("y_test shape: ",y_test.shape)
+
+    clf = make_pipeline(StandardScaler(), svm.SVC(cache_size=1000))
+    clf.fit(X_train, y_train)
+
+    y_pred = clf.predict(X_test)
+    print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
 
 
 
+ 
+
+    
 
 
 main()
